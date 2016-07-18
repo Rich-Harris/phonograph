31f8cf085d1e682e5b18a3745d37f3b94aff16d1
Rich Harris <richard.a.harris@gmail.com>
2016-07-18 13:43:54 -0400

    init on ios

--- src/index.js
+++ src/index.js
@@ -2,3 +2,4 @@ import './Clone.js'; // need to import Clone before Clip
 
 export { default as Clip } from './Clip.js';
 export { default as getContext } from './getContext.js';
+export { default as init } from './init.js';
---
+++ src/init.js
@@ -0,0 +1,29 @@
+import getContext from './getContext.js';
+
+let inited;
+
+window.addEventListener( 'touchend', init, false );
+
+// https://paulbakaus.com/tutorials/html5/web-audio-on-ios/
+export default function init () {
+	if ( inited ) return;
+
+	const context = getContext();
+
+	// create a short empty buffer
+	var buffer = context.createBuffer( 1, 1, 22050 );
+	var source = context.createBufferSource();
+	source.buffer = buffer;
+	source.connect( context.destination );
+
+	source.start();
+
+	setTimeout( () => {
+		if ( !inited ) {
+			if ( source.playbackState === source.PLAYING_STATE || source.playbackState === source.FINISHED_STATE ) {
+				inited = true;
+				window.removeEventListener( 'touchend', init, false );
+			}
+		}
+	});
+}
