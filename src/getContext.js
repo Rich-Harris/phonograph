68e0c62b4c32265827eca16a365ea54c70e3190b
Rich Harris <richard.a.harris@gmail.com>
2017-11-12 08:47:29 -0500

    remove generated .js files

--- src/getContext.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var context;
-function getContext() {
-    return (context ||
-        (context = new (typeof AudioContext !== 'undefined' ? AudioContext : webkitAudioContext)()));
-}
-exports.default = getContext;
--- src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./Clone.js"); // need to import Clone before Clip
-var Clip_1 = require("./Clip");
-exports.Clip = Clip_1.default;
-var getContext_1 = require("./getContext");
-exports.getContext = getContext_1.default;
-var init_1 = require("./init");
-exports.init = init_1.default;
